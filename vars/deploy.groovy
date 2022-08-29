9209c20d6ae642c5309e1158f8731971cdcab780
subrata-hazra <subratah@gmail.com>
2022-08-29 18:07:20 +0530

    Implementing generic pipeline

--- vars/deploy.groovy
+++ vars/deploy.groovy
@@ -14,7 +14,6 @@ def call() {
                 }
             }
             stage('test') {
-                echo
                 steps {
                     script {
                         println('application.name= ' + appName)
