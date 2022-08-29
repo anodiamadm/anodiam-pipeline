f2925c8a915e5bec3327ff8e642b75b71cd516ff
subrata-hazra <subratah@gmail.com>
2022-08-29 18:08:21 +0530

    Implementing generic pipeline

--- vars/deploy.groovy
+++ vars/deploy.groovy
@@ -4,13 +4,15 @@ def call() {
         agent any;
         stages {
             stage('init') {
-                def files = findFiles(glob: 'cicd.yaml')
-                if(files.length > 0) {
-                    println('Found ' + files[0])
-                    def datas = readYaml (file: "${WORKSPACE}/" + files[0])
-                    appName = datas.application.name
-                } else {
-                    error "Pipeline not configured. Please configure using cicd.yaml"
+                steps {
+                    def files = findFiles(glob: 'cicd.yaml')
+                    if (files.length > 0) {
+                        println('Found ' + files[0])
+                        def datas = readYaml(file: "${WORKSPACE}/" + files[0])
+                        appName = datas.application.name
+                    } else {
+                        error "Pipeline not configured. Please configure using cicd.yaml"
+                    }
                 }
             }
             stage('test') {
