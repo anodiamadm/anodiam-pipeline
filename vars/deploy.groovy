f49723f6e4aaa165f9978474e8a380af9264158e
subrata-hazra <subratah@gmail.com>
2022-08-29 18:06:06 +0530

    Implementing buildpack

--- vars/deploy.groovy
+++ vars/deploy.groovy
@@ -1,18 +1,23 @@
 def call() {
+    def appName = ''
     pipeline {
         agent any;
         stages {
+            stage('init') {
+                def files = findFiles(glob: 'cicd.yaml')
+                if(files.length > 0) {
+                    println('Found ' + files[0])
+                    def datas = readYaml (file: "${WORKSPACE}/" + files[0])
+                    appName = datas.application.name
+                } else {
+                    error "Pipeline not configured. Please configure using cicd.yaml"
+                }
+            }
             stage('test') {
+                echo
                 steps {
                     script {
-                        def files = findFiles(glob: 'cicd.yaml')
-                        if(files.length > 0) {
-                            println('Found ' + files[0])
-                            def datas = readYaml (file: "${WORKSPACE}/" + files[0])
-                            println('application.name= ' + datas.application.name)
-                        } else {
-
-                        }
+                        println('application.name= ' + appName)
                     }
                 }
             }
