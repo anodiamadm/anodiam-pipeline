01e5e88b7e0429fcb836b1cd365949cb0f1d430f
subrata-hazra <subratah@gmail.com>
2022-08-30 15:58:53 +0530

    Implementing generic pipeline

--- vars/deploy.groovy
+++ vars/deploy.groovy
@@ -1,31 +1,106 @@
-def call() {
+def call(String buildPack = 'maven') {
     def config=[:]
-    def buildPack = 'maven'
     def podTemplate = """"""
-
-    try {
-        if (fileExists('cicd.yaml')) {
-            config = readYaml(file: "${WORKSPACE}/cicd.yaml")
-            buildPack = config.application.buildPack
-            if('maven' == buildPack) {
-                podTemplate = getMavenPodTemplate()
-            } else if('gradle' == buildPack) {
-                podTemplate = getGradlePodTemplate()
-            } else if('npm' == buildPack) {
-                podTemplate = getNPMPodTemplate()
-            } else {
-                error "Buildpack not defined/implemented"
-            }
-        } else {
-            error "Pipeline not configured. Please configure using cicd.yaml"
-        }
-    } catch(Exception e) {
-        error "Pipeline failed, ERROR: " + e.getMessage()
-    }
+    def project = ''
+    def appName = ''
+    def namespace = ''
+    def clusterName = ''
+    def clusterRegion = ''
+    def clusterZone = ''
+    def imageTag = ''
 
     pipeline {
-        agent any;
+        agent {
+            kubernetes {
+                label "${config.appName}"
+                defaultContainer 'jnlp'
+                yaml getPodTemplate(buildPack)
+            }
+        }
+
         stages {
+            stage('Init') {
+                steps {
+                    script {
+                        try {
+                            if (fileExists('cicd.yaml')) {
+                                config = readYaml(file: "${WORKSPACE}/cicd.yaml")
+                                def envConfig = config.branch.feature
+                                project =
+                                appName = config.application.name
+                                imageTag = "${CLUSTER_REGION}-docker.pkg.dev/${PROJECT}/anodiam-repo/${APP_NAME}:v${env.BUILD_NUMBER}"
+                            } else {
+                                error "Pipeline not configured. Please configure using cicd.yaml"
+                            }
+                        } catch(Exception e) {
+                            error "Pipeline failed, ERROR: " + e.getMessage()
+                        }
+                    }
+                }
+            }
+            stage('Environment Configuration') {
+                steps {
+                    script {
+                        def branchName = scm.branches[0].name.split("/")[1]
+                        println("Branch Name = " + branchName)
+                    }
+                    sh 'printenv'
+                }
+            }
+            stage('Build Artifact') {
+                steps {
+                    container("${buildPack}") {
+                        sh("mkdir artifact")
+                        script {
+                            if('maven' == buildPack) {
+                                sh("mvn clean package")
+                                sh("cp target/*.jar artifact")
+                            } else if('gradle' == buildPack) {
+                                sh("gradle clean build")
+                                sh("cp build/libs/*.jar artifact")
+                            } else if('npm' == buildPack) {
+                                sh("npm install --omit=dev")
+                                sh("npm run build")
+                                sh("cp -r build/* artifact")
+                            } else {
+                                error "Buildpack not defined/implemented"
+                            }
+
+                            def envConfig = config.branch.feature
+                            if (envConfig.manifestDir) {
+                                sh("cp ${envConfig.manifestDir}/Dockerfile artifact")
+                            } else {
+                                sh("cp Dockerfile artifact")
+                            }
+                        }
+                    }
+                }
+            }
+            stage('Push image with Container Builder') {
+                steps {
+                    container('gcloud') {
+                        sh("PYTHONUNBUFFERED=1 gcloud builds submit -t ${IMAGE_TAG} ./artifact")
+                    }
+                }
+            }
+            stage('Deploy Dev') {
+                // Feature branch
+                when { branch 'feature/**' }
+                steps {
+                    container('kubectl') {
+                        sh("PYTHONUNBUFFERED=1 gcloud container clusters get-credentials ${CLUSTER_NAME} --zone=${CLUSTER_ZONE}")
+                        script {
+                            if ("${config.manifestDir}") {
+                                sh("sed -i.bak 's#APP_IMAGE#${IMAGE_TAG}#' ${config.manifestDir}/*.yaml")
+                                sh("kubectl apply -n ${TARGET_NAMESPACE} -f ${config.manifestDir}")
+                            } else {
+                                sh("sed -i.bak 's#APP_IMAGE#${IMAGE_TAG}#' ./k8s/*.yaml")
+                                sh("kubectl apply -n ${TARGET_NAMESPACE} -f ./k8s")
+                            }
+                        }
+                    }
+                }
+            }
             stage('test feature') {
                 when { branch 'feature/**' }
                 steps {
@@ -46,6 +121,18 @@ def call() {
                 }
             }
         }
+    }
+}
+
+static String getPodTemplate(String buildPack = 'maven') {
+    if('maven' == buildPack) {
+        return getMavenPodTemplate()
+    } else if('gradle' == buildPack) {
+        return getGradlePodTemplate()
+    } else if('npm' == buildPack) {
+        return getNPMPodTemplate()
+    } else {
+        error "Buildpack not defined/implemented"
     }
 }
 
