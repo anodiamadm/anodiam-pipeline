6b7cfb3fdbc667213c1fd4a9eaf2de419dee1c1c
subrata-hazra <subratah@gmail.com>
2022-08-30 20:11:59 +0530

    Implementing generic pipeline

--- vars/deploy.groovy
+++ vars/deploy.groovy
@@ -56,15 +56,15 @@ def call(String buildPack = 'maven', String appName = 'app-name-not-specified') 
                                 println("Selected deployment type = " + deploymentType)
                             } else {
                                 println("Selected deployment type = " + deploymentType)
-                                def currentImage = sh(script: "kubectl get deployment " + appName + " -n ${deploymentConfig.namespace} -o=jsonpath='{\$.spec.template.spec.containers[:1].image}'", returnStdout: true)
-                                if(!currentImage.startsWith('Error')) {
+                                try {
+                                    def currentImage = sh(script: "kubectl get deployment " + appName + " -n ${deploymentConfig.namespace} -o=jsonpath='{\$.spec.template.spec.containers[:1].image}'", returnStdout: true)
                                     println("Found app image = " + currentImage)
                                     def rollbackImageTag = input(id: 'rollbackImageTag', message: 'Please Select Image Tag to Rollback',
                                             parameters: [[$class: 'ChoiceParameterDefinition', description:'', name:'', choices: "New Build" + "\n" + currentImage]
                                             ])
                                     println("Selected app image = " + rollbackImageTag)
-                                } else {
-                                    error currentImage
+                                } catch(Exception e) {
+                                    error "Pipeline failed, ERROR: " + e.getMessage()
                                 }
                             }
                         }
