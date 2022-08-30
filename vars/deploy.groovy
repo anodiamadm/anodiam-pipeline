855b11eb7582077d1beabec17cf0158fa288f4d3
subrata-hazra <subratah@gmail.com>
2022-08-30 16:54:43 +0530

    Implementing generic pipeline

--- vars/deploy.groovy
+++ vars/deploy.groovy
@@ -1,8 +1,7 @@
-def call(String buildPack = 'maven') {
+def call(String buildPack = 'maven', String appName = 'app-name-not-specified') {
     def config=[:]
     def podTemplate = getPodTemplate(buildPack)
     def project = ''
-    def appName = ''
     def namespace = ''
     def clusterName = ''
     def clusterRegion = ''
@@ -12,7 +11,7 @@ def call(String buildPack = 'maven') {
     pipeline {
         agent {
             kubernetes {
-                label "${config.appName}"
+                label "${appName}"
                 defaultContainer 'jnlp'
                 yaml "${podTemplate}"
             }
@@ -25,10 +24,6 @@ def call(String buildPack = 'maven') {
                         try {
                             if (fileExists('cicd.yaml')) {
                                 config = readYaml(file: "${WORKSPACE}/cicd.yaml")
-                                def envConfig = config.branch.feature
-                                project =
-                                appName = config.application.name
-                                imageTag = "${CLUSTER_REGION}-docker.pkg.dev/${PROJECT}/anodiam-repo/${APP_NAME}:v${env.BUILD_NUMBER}"
                             } else {
                                 error "Pipeline not configured. Please configure using cicd.yaml"
                             }
@@ -43,83 +38,14 @@ def call(String buildPack = 'maven') {
                     script {
                         def branchName = scm.branches[0].name.split("/")[1]
                         println("Branch Name = " + branchName)
+                        //def envConfig = config.branch.feature
+                        //appName = config.application.name
+                        //imageTag = "${CLUSTER_REGION}-docker.pkg.dev/${PROJECT}/anodiam-repo/${APP_NAME}:v${env.BUILD_NUMBER}"
                     }
                     sh 'printenv'
                 }
             }
-            stage('Build Artifact') {
-                steps {
-                    container("${buildPack}") {
-                        sh("mkdir artifact")
-                        script {
-                            if('maven' == buildPack) {
-                                sh("mvn clean package")
-                                sh("cp target/*.jar artifact")
-                            } else if('gradle' == buildPack) {
-                                sh("gradle clean build")
-                                sh("cp build/libs/*.jar artifact")
-                            } else if('npm' == buildPack) {
-                                sh("npm install --omit=dev")
-                                sh("npm run build")
-                                sh("cp -r build/* artifact")
-                            } else {
-                                error "Buildpack not defined/implemented"
-                            }
 
-                            def envConfig = config.branch.feature
-                            if (envConfig.manifestDir) {
-                                sh("cp ${envConfig.manifestDir}/Dockerfile artifact")
-                            } else {
-                                sh("cp Dockerfile artifact")
-                            }
-                        }
-                    }
-                }
-            }
-            stage('Push image with Container Builder') {
-                steps {
-                    container('gcloud') {
-                        sh("PYTHONUNBUFFERED=1 gcloud builds submit -t ${IMAGE_TAG} ./artifact")
-                    }
-                }
-            }
-            stage('Deploy Dev') {
-                // Feature branch
-                when { branch 'feature/**' }
-                steps {
-                    container('kubectl') {
-                        sh("PYTHONUNBUFFERED=1 gcloud container clusters get-credentials ${CLUSTER_NAME} --zone=${CLUSTER_ZONE}")
-                        script {
-                            if ("${config.manifestDir}") {
-                                sh("sed -i.bak 's#APP_IMAGE#${IMAGE_TAG}#' ${config.manifestDir}/*.yaml")
-                                sh("kubectl apply -n ${TARGET_NAMESPACE} -f ${config.manifestDir}")
-                            } else {
-                                sh("sed -i.bak 's#APP_IMAGE#${IMAGE_TAG}#' ./k8s/*.yaml")
-                                sh("kubectl apply -n ${TARGET_NAMESPACE} -f ./k8s")
-                            }
-                        }
-                    }
-                }
-            }
-            stage('test feature') {
-                when { branch 'feature/**' }
-                steps {
-                    script {
-                        def envConfig = config.branch.feature
-                        println('Application Name=' + config.application.name)
-                        println('namespace=' + envConfig.namespace)
-                        println('pod template=' + podTemplate)
-                    }
-                }
-            }
-            stage('test develop') {
-                when { branch 'develop' }
-                steps {
-                    script {
-                        println('application.name=' + config.application.name)
-                    }
-                }
-            }
         }
     }
 }
